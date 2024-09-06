30325cf86b9ce58453c1f1d83ca282c664cbf82c
CKShetty4 <ckshetty004@gmail.com>
2024-09-06 22:39:39 +0530

    Project Initial Setup

---
+++ app/_layout.tsx
@@ -0,0 +1,8 @@
+import {View, Text} from 'react-native';
+export default function RootLayout() {
+  return (
+    <View className='bg-black h-full'>
+      <Text className='text-3xl text-white' >RootLayout</Text>
+    </View>
+  );
+}

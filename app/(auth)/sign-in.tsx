9f683f7a9eb9f8651a3d0c32ca304d4b3feae7a7
CKShetty4 <ckshetty004@gmail.com>
2024-09-08 16:15:01 +0530

    Welcome Page and Custum button

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,11 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className='bg-primary-100 h-full'>
+      <View>
+        <Text className='text-black'>Sign In</Text>
+      </View>
+    </SafeAreaView>
   )
 }
 
--- app/index.tsx
+++ app/index.tsx
@@ -1,48 +1,71 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { StatusBar } from "expo-status-bar";
+import { ScrollView, View, Text, Image, ImageBackground } from "react-native";
 import { Redirect, router } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import CustomButton from "../components/custombutton";
+import { images } from "@/constants";
+
 
 
 const Index = () => {
   return (
-    <View>
-      <Text>Index </Text>
-      <Redirect href="/(tabs)/inbox"/>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <View>
+        <Image
+          source={images.WelcomeBG}
+          className="w-full h-[100%] absolute"
+          style={{opacity: 0.4}}
+        />
+        <ScrollView
+        className=" mt-[55vh]"
+          contentContainerStyle={{
+            height: "100%",
+          }}
+        >
+          
+          <View className="mr-10 ml-10">
+          <View className="items-center">
+          <Text className="text-black font-HowdyParty text-3xl">Welcome! </Text>
+          <Text className="text-black font-psemibold text-3xl ">
+          Create, Connect and discover with your local community and beyond.
+          </Text></View>
+            <CustomButton
+              title="Continue with Email"
+              handlePress={() => router.replace('/sign-in')}
+              containerStyles="w-full mt-7 "
+              textStyles="text-black font-bold"
+            />
+          </View>
+        </ScrollView>
+      </View>
+    </SafeAreaView>
   )
 }
 
 export default Index
 
 
-  /* import { StatusBar } from "expo-status-bar";
-import { ScrollView, View,Text,Image } from "react-native";
-import { Redirect, router } from "expo-router";
-import { SafeAreaView } from "react-native-safe-area-context";
+/*
 import {images} from '../constants';
-import CustomButton from "@/components/CustomButton";
+
 import { useGlobalContext } from "@/context/GlobalProvider";
 
 // import 'react-native-url-polyfill/auto';
 
 export default function App() {
-  const {isLoading,isLoggedIn} = useGlobalContext();
+const {isLoading,isLoggedIn} = useGlobalContext();
 
 if(!isLoading && isLoggedIn){ return <Redirect href="/home"/>}
 
-  return (
-    <SafeAreaView className="bg-primary h-full">
-     <ScrollView
-        contentContainerStyle={{
-          height: "100%",
-        }}
-      >
+return (
+  
+   
 <View className="w-full justify-center items-center min-h-[85vh] px-4">
 <Image source={images.logo} className="w-[130px] h-[84px]" resizeMode="contain"/>
 <Image source={images.cards} className="max-w-[380px] w-full h-[300px]" resizeMode="contain"/>
 <View className="relative mt-5">
 <Text className="text-3xl text-white font-bold text-center">Discover Endless posibilities with {' '}
-  <Text className="text-secondary-200">GenSnap</Text>
+<Text className="text-secondary-200">GenSnap</Text>
 </Text>
 <Image 
 source={images.path}
@@ -50,15 +73,10 @@ className="w-[140px] h-[15] absolute -bottom-2 -right-1" resizeMode="contain"/>
 
 </View>
 <Text className="text-gray-100 mt-7 text-sm font-pregular text-center">Where creativity meets innovation: embark on a journey of limitless exploration with GenSnap</Text>
-<CustomButton
-  title="Continue with Email"
-  handlePress={()=>router.push('/sign-in')}
-  containerStyles="w-full mt-7"/>
-</View>
-</ScrollView>
 
-<StatusBar backgroundColor="#161622" style="light" />
-</SafeAreaView>
-  );
+</View>
+
+
+);
 }
-  */
+*/

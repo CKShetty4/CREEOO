3f4baacb3755da82effd8f3bf161a030fe665544
CKShetty4 <ckshetty004@gmail.com>
2024-09-11 22:26:22 +0530

    NearBy tab Outline made

--- app/(tabs)/nearby.tsx
+++ app/(tabs)/nearby.tsx
@@ -1,11 +1,24 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { icons, images } from '@/constants'
 
 const Nearby = () => {
   return (
-    <View>
-      <Text>Nearby</Text>
-    </View>
+    <SafeAreaView className='bg-primary-100 h-full '>
+    <Text className='font-HowdyParty text-3xl mt-1 text-center'>NearBy</Text>
+  
+<View className='flex-1'>
+  <View className='absolute bottom-5 right-5'> 
+    <Image 
+      source={icons.people}
+      className='h-30 w-30'
+      resizeMode='contain'
+    />
+  </View>
+</View>
+
+    </SafeAreaView>
   )
 }
 

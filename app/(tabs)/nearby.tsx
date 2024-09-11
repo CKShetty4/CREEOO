ab529a15629090209ec3fd52fda7d27dd1bd3726
CKShetty4 <ckshetty004@gmail.com>
2024-09-11 22:36:09 +0530

    Making Profile Section

--- app/(tabs)/nearby.tsx
+++ app/(tabs)/nearby.tsx
@@ -7,7 +7,10 @@ const Nearby = () => {
   return (
     <SafeAreaView className='bg-primary-100 h-full '>
     <Text className='font-HowdyParty text-3xl mt-1 text-center'>NearBy</Text>
-  
+    <View>
+
+
+    </View>
 <View className='flex-1'>
   <View className='absolute bottom-5 right-5'> 
     <Image 
--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,11 +1,20 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Profile = () => {
   return (
+    <SafeAreaView className='bg-primary-100 h-full '>
+    <Text className='font-HowdyParty text-3xl mt-1 text-center'>Profile</Text>
     <View>
-      <Text>Profile</Text>
+      <ScrollView>
+
+      <View className=' my-4 ml-3 mr-3 border-b border-black'></View>
+      </ScrollView>
     </View>
+
+
+    </SafeAreaView>
   )
 }
 
